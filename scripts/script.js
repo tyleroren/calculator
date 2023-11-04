cbebea280a0ef74489571804ca7d24defa34e1d8
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-04 17:29:49 -0400

    Add buttons and initial css

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,7 @@
+let userA = 0;
+let userB = 0;
+let userOp = null;
+
 function add(a, b) {
     return a + b;
 }
@@ -12,5 +16,25 @@ function multiply(a, b) {
 
 function divide(a, b) {
     return a / b;
+}
+
+
+function calculate(a, b, op) {
+    switch (op) {
+        case "+":
+            return add(a, b);
+            break;
+        case "-":
+            return subtract(a, b);
+            break;
+        case "*":
+            return multiply(a, b);
+            break;
+        case "/":
+            return divide(a, b);
+            break;
+        default:
+            return "oops";
+    }
 }
 
