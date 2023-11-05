18e93a82045f90101601c1c2dc9b702376c7e6bd
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-05 18:23:12 -0500

    Add more features to functions
    Added error checking for chained operands
    Added keyboard shortcuts for several of the buttons
    Adjusted clickedX functions to allow for keyboard
    Prevent multiple equal signs when spamming equal key
    Started adjusting operand function to allow for changing operators

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,11 @@
 let userA = 0;
 let userB = 0;
 let userOp = null;
+let clearText = 0;
 const displayElement = document.querySelector('#display');
 const historyElement = document.querySelector('#history');
 let displayNumber = Number(displayElement.textContent);
+
 
 function calculate(a, b, op) {
     switch (op) {
@@ -25,35 +27,57 @@ function calculate(a, b, op) {
     }
 }
 
-function appendDisplay(num) {
+function appendDisplay(e) {
     // adds the clicked number to the end of the display, stores new value as a mathable variable 
-    displayElement.textContent += num;
+    // checks to see if input is key or mouse, which changes how the text is added
+    if (clearText == 1) {displayElement.textContent = ""};
+    if (e.key) {
+        displayElement.textContent += e.key;
+    } else {
+        displayElement.textContent += e.target.value;
+    }
     displayNumber = Number(displayElement.textContent);
+    clearText = 0;
 }
 
-function clickOperand(op) {
-    if (userOp !== null && displayElement.textContent !== "") {
-        userOp = op.value;
+function clickOperand(e) {
+    let inputTxt = 0;
+    let inputOp = 0;
+    // splitting up mouse vs key, sets vars depending on what is used
+    if (e.key) {
+        inputTxt = e.key;
+        inputOp = e.key;
+    } else {
+        inputTxt = e.target.textContent;
+        inputOp = e.target.value;
+    }
+    if (userOp !== null && displayElement.textContent !== "" && displayNumber !== "") {
+        // performs the calculation if there is already an existing equation
+        // Updates the display and history with calculation outcome
         displayElement.textContent = calculate(userA, displayNumber, userOp);
-        historyElement.textContent = `${displayElement.textContent} ${op.textContent} `;
+        historyElement.textContent = `${displayElement.textContent} ${inputTxt} `;
         displayNumber = Number(displayElement.textContent);
-        userA = displayNumber;
-    }
-
-    if (userOp === null) {
+    } else if (userOp === null) {
         // moves the displayed number to the history line and adds the operand to the end
-        historyElement.textContent = `${displayNumber} ${op.textContent} `;
-        // store the number and operand separately for mathing later, clear variables 
-        userOp = op.value;
-        userA = displayNumber;
+        displayNumber = Number(displayElement.textContent);
+        historyElement.textContent = `${displayNumber} ${inputTxt} `;
         displayElement.textContent = "";
-        displayNumber = 0;
+    } else {
+        historyElement.textContent = "";
+        displayElement.textContent = "error";
     }
+    // updates the operand with the one that was just clicked
+    // updates the user variable with the newly displayed number
+    userOp = inputOp;
+    clearText = 1;
+    userA = displayNumber;
+    displayNumber = "";
 }
 
 function clickEquals() {
-    historyElement.textContent += `${displayNumber} = `;
+    if (historyElement.textContent.includes('=')) {historyElement.textContent += `${displayNumber} = `};
     displayElement.textContent = calculate(userA, displayNumber, userOp);
+    clearText = 1;
 }
 
 function clickClear() {
@@ -64,7 +88,21 @@ function clickClear() {
     displayNumber = 0;
 }
 
-document.querySelectorAll('.number').forEach((item) => item.addEventListener('click', () => appendDisplay(item.value)));
-document.querySelector('.clear').addEventListener('click', () => clickClear());
-document.querySelectorAll('.operand').forEach((item) => item.addEventListener('click', () => clickOperand(item)));
-document.querySelector('.compute').addEventListener('click', () => clickEquals());
+numShortcuts = ['.', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+opShortcuts = ['+', '-', '*', '/'];
+
+document.querySelectorAll('.number').forEach((item) => item.addEventListener('click', appendDisplay));
+document.querySelector('.clear').addEventListener('click', clickClear);
+document.querySelectorAll('.operand').forEach((item) => item.addEventListener('click', clickOperand));
+document.querySelector('.compute').addEventListener('click', clickEquals);
+
+document.addEventListener('keyup', (e) => {
+    console.log(e);
+    if (numShortcuts.includes(e.key)) {
+        appendDisplay(e);
+    } else if (opShortcuts.includes(e.key)) {
+        clickOperand(e);
+    } else if (e.key === "Enter") {
+        clickEquals(e);
+    }
+});
