01fe1865b892a5c72703ca2c4f102039cd6d4ad9
Tyler Oren <131497714+tyleroren@users.noreply.github.com>
2023-11-05 12:04:36 -0500

    Add more complex functions
    Can do basic math with the operators but no error checking
    Adding advanced functionality like multiple operands
    Starting to add error checking but not all working yet

--- scripts/script.js
+++ scripts/script.js
@@ -2,46 +2,69 @@ let userA = 0;
 let userB = 0;
 let userOp = null;
 const displayElement = document.querySelector('#display');
-
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
+const historyElement = document.querySelector('#history');
+let displayNumber = Number(displayElement.textContent);
 
 function calculate(a, b, op) {
     switch (op) {
         case "+":
-            return add(a, b);
+            return a + b;
             break;
         case "-":
-            return subtract(a, b);
+            return a - b;
             break;
-        case "*":
-            return multiply(a, b);
+        case "x":
+            return a * b;
             break;
         case "/":
-            return divide(a, b);
+            return a / b;
             break;
         default:
+            console.log(op);
             return "oops";
     }
 }
 
 function appendDisplay(num) {
+    // adds the clicked number to the end of the display, stores new value as a mathable variable 
     displayElement.textContent += num;
+    displayNumber = Number(displayElement.textContent);
+}
+
+function clickOperand(op) {
+    if (userOp !== null && displayElement.textContent !== "") {
+        userOp = op.value;
+        displayElement.textContent = calculate(userA, displayNumber, userOp);
+        historyElement.textContent = `${displayElement.textContent} ${op.textContent} `;
+        displayNumber = Number(displayElement.textContent);
+        userA = displayNumber;
+    }
+
+    if (userOp === null) {
+        // moves the displayed number to the history line and adds the operand to the end
+        historyElement.textContent = `${displayNumber} ${op.textContent} `;
+        // store the number and operand separately for mathing later, clear variables 
+        userOp = op.value;
+        userA = displayNumber;
+        displayElement.textContent = "";
+        displayNumber = 0;
+    }
+}
+
+function clickEquals() {
+    historyElement.textContent += `${displayNumber} = `;
+    displayElement.textContent = calculate(userA, displayNumber, userOp);
+}
+
+function clickClear() {
+    displayElement.textContent = "";
+    historyElement.textContent = "";
+    userOp = null;
+    userA = 0;
+    displayNumber = 0;
 }
 
 document.querySelectorAll('.number').forEach((item) => item.addEventListener('click', () => appendDisplay(item.value)));
-document.querySelector('.clear').addEventListener('click', () => displayElement.textContent = "");
+document.querySelector('.clear').addEventListener('click', () => clickClear());
+document.querySelectorAll('.operand').forEach((item) => item.addEventListener('click', () => clickOperand(item)));
+document.querySelector('.compute').addEventListener('click', () => clickEquals());
